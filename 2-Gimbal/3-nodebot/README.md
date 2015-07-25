776109cdb8a3f9a278cc469e349c122824d6c4b0
Martin Andrews <GitHub@mdda.net>
2015-07-25 16:04:59 +0800

    package file for npm

--- 2-Gimbal/3-nodebot/README.md
+++ 2-Gimbal/3-nodebot/README.md
@@ -28,5 +28,39 @@ node gimbal.js
 
 * Blue LED : PIN 8
 
-* 
+* PWM PINS : {3, 5, 6, 9, 10, 11}  (confirmed in ```BLcontroller.h```) :
+```
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  
+  // Enable Timer1 Interrupt for Motor Control
+  OCR2A = 0;  //D11 APIN
+  OCR2B = 0;  //D3
+  OCR1A = 0;  //D9  CPIN
+  OCR1B = 0;  //D10 BPIN
+  OCR0A = 0;  //D6
+  OCR0B = 0;  //D5 
+```
+
+And, from ```definitions.h``` :
+```
+// Hardware Abstraction for Motor connectors,
+// DO NOT CHANGE UNLES YOU KNOW WHAT YOU ARE DOING !!!
+#define PWM_A_MOTOR1 OCR2A
+#define PWM_B_MOTOR1 OCR1B
+#define PWM_C_MOTOR1 OCR1A
+
+#define PWM_A_MOTOR0 OCR0A
+#define PWM_B_MOTOR0 OCR0B
+#define PWM_C_MOTOR0 OCR2B
+```
+
+Therefore : 
+
+* PWM pins for 1 motor : [A,B,C]=[11, 10, 9]
+* PWM pins for 0 motor : [A,B,C]=[ 6,  5, 3]
 
--- 2-Gimbal/3-nodebot/gimbal.js
+++ 2-Gimbal/3-nodebot/gimbal.js
@@ -1,21 +1,58 @@
 
-
-
-// https://github.com/rwaldron/johnny-five
-// https://github.com/rwaldron/johnny-five/wiki/Board
-// https://www.arduino.cc/en/Main/ArduinoBoardPro
-
-
-// npm install johnny-five
-
 var five = require("johnny-five");
+
+var keypress = require("keypress");
+keypress(process.stdin);
+
 var board = new five.Board();
 
 // The board's pins will not be accessible until
 // the board has reported that it is ready
+
 board.on("ready", function() {
   console.log("Ready!");
 
   var led = new five.Led(8);
-  led.blink(100);
+  //led.blink(100);
+
+  console.log("Use Up and Down arrows for CW and CCW respectively. Space to stop.");
+
+/*
+// Hardware Abstraction for Motor connectors,
+// DO NOT CHANGE UNLES YOU KNOW WHAT YOU ARE DOING !!!
+#define PWM_A_MOTOR1 OCR2A
+#define PWM_B_MOTOR1 OCR1B
+#define PWM_C_MOTOR1 OCR1A
+
+#define PWM_A_MOTOR0 OCR0A
+#define PWM_B_MOTOR0 OCR0B
+#define PWM_C_MOTOR0 OCR2B
+*/
+
+  var servo = new five.Servo.Continuous(10).stop();
+
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  process.stdin.setRawMode(true);
+
+  process.stdin.on("keypress", function(ch, key) {
+
+    if (!key) {
+      return;
+    }
+
+    if (key.name === "q") {
+      console.log("Quitting");
+      process.exit();
+    } else if (key.name === "up") {
+      console.log("CW");
+      servo.cw();
+    } else if (key.name === "down") {
+      console.log("CCW");
+      servo.ccw();
+    } else if (key.name === "space") {
+      console.log("Stopping");
+      servo.stop();
+    }
+  });
 });
