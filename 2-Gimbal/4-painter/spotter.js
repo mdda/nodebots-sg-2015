ffa6888700acb02d4b8325cbbbf7debfbd97bdd3
Martin Andrews <GitHub@mdda.net>
2015-08-02 01:53:23 +0800

    Nice rectangle around detected point

--- 2-Gimbal/4-painter/spotter.js
+++ 2-Gimbal/4-painter/spotter.js
@@ -21,17 +21,42 @@ function get_frame() {
     t1=t2;
     
     if (im.size()[0] > 0 && im.size()[1] > 0) {
-      viewer.show(im);
-      console.log("Image displayed");
+      //viewer.show(im);
+      //console.log("Image displayed");
+      var processed = im.copy();
+      //processed.convertHSVscale();
       
-      //pub.send("123123");
-      pub.send(JSON.stringify({ a:'to', xy:[0.1,0.2] }));
+      var channel = processed.split();
+      console.log("Channel.length : ", channel.length);
+      // 0=Blue, 1=Green, 2=Red (?)
+      // H S V
+      
+/*
+// (B)lue, (G)reen, (R)ed
+var lower_threshold = [46, 57, 83];
+var upper_threshold = [80, 96, 115];
+
+im.inRange(lower_threshold, upper_threshold);
+*/  
+      
+      var chan=channel[0];
+      var o = chan.minMaxLoc();
+      console.log(o);
+      
+      // Alternative : http://stackoverflow.com/questions/4010036/laser-light-detection-with-opencv-and-c
+      // http://docs.opencv.org/2.4.9/modules/core/doc/operations_on_arrays.html?highlight=minmaxloc#inrange
+      
+      var rsize=20;
+      chan.rectangle( [o.maxLoc.x-rsize/2, o.maxLoc.y-rsize/2], [rsize, rsize], 200, 1);
+      
+      viewer.show(chan);
+      
+      //pub.send(JSON.stringify({ a:'to', xy:[0.1,0.2] }));
     }
     else {
       console.log("No Image returned");
     }
     viewer.blockingWaitKey(1);
-    //viewer.blockingWaitKey(-1);
   });
 }
 
